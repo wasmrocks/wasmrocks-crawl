5227c254893afc356663ee2212dc737f65b7bf3d
unbug <tidelgl@gamil.com>
2018-07-22 17:25:59 +0800

    parse text and author

--- medium/spiders/TagWebassembly.py
+++ medium/spiders/TagWebassembly.py
@@ -12,8 +12,9 @@ class TagWebassemblySpider(scrapy.Spider):
             if item.css(".section-content h3::text").extract_first():
                 yield {
                     'title': item.css(".section-content h3::text").extract_first(),
-                    'text': item.css(".section-inner::text").extract_first(),
-                    'datetime': item.css(".postMetaInline time::attr(datatime)").extract_first(),
+                    'text': '<br>'.join(item.css(".postArticle-content .section-inner>*::text").extract()),
+                    'datetime': item.css(".postMetaInline time::attr(datetime)").extract_first(),
                     'link': item.css(".postArticle-readMore>a::attr(href)").extract_first().split("?")[0],
-                    'author_name': item.css(".postMetaInline-authorLockup>a::attr(href)").extract_first().split("?")[0],
+                    'author_name': item.css(".postMetaInline-authorLockup>a::text").extract_first(),
+                    'author_url': item.css(".postMetaInline-authorLockup>a::attr(href)").extract_first().split("?")[0],
                 }
